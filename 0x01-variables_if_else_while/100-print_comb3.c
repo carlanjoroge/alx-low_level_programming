69825040e399f547d83dede6d281c10ef6e52c8b
carlanjoroge <carlanjoroge16@gmail.com>
2023-02-18 02:17:41 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: write a program that prints all possible
+ * different combinations of two digits
+ * numbers must be separated by ,, followed by a space
+ * the two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * prints only the smallest combination of two digits
+ * numbers should be prited in ascending order, with two digits
+ * you can only use the putchar function, a maximum of 5 times in your code
+ * You are not allowed to use any variable of type char
+ * all you code should be in the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(');
+					putchar( );
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
