e4d0bbf33ce73cf791a70e7dab77deeb8a826dc5
carlanjoroge <carlanjoroge16@gmail.com>
2023-03-07 01:26:54 -0800

    strspn

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,20 +3,19 @@
  * _strpbrk - Entry point
  * @s: input
  * @accept: input
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-		if (*S == accept[k])
-		return (s);
-		}
-	s++;
-	}
-	return ('\0');
+int k;
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+return ('\0');
 }
