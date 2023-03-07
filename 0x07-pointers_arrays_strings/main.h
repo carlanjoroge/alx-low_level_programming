cbd6527a9ee094d9a452a3ce94ccf87824c10484
carlanjoroge <carlanjoroge16@gmail.com>
2023-03-07 01:05:43 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
+/**
+ * File: main.h
+ * description: Header file containing prototypes for all functions
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
